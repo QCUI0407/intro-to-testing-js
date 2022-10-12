21ea442613edfbf2f01c60a8b71c53637c114c9c
Qiyuan Cui <cuiqiyuan1989@gmail.com>
2022-10-12 14:38:11 -0500

    add tests and ability to say 'hello' to any input.

--- tests.js
+++ tests.js
@@ -14,6 +14,38 @@ describe('helloWorld', function() {
     });
 });
 
-describe(it("Is a defined function", function (){
+describe("sayHello",function () {
+    it("Is a defined function", function (){
     expect(typeof sayHello).toBe("function");
-}))
+});
+    it("should return a string when called.",function (){
+    expect(typeof sayHello()).toBe("string");
+    });
+    it('should return the string "Hello, Jane!" ', function() {
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
+    });
+    it('should return the string "Hello, Alex!" ', function() {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
+    });
+    it('should return the string "Hello, Pat!" ', function() {
+        expect(sayHello("Pat")).toBe("Hello, Pat!");
+    });
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
